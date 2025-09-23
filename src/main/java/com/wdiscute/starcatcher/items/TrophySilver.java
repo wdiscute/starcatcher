6d3c763fff3807de873095b3a4d70f1d83b0e1c7
wd <lucasalvesbispo2@gmail.com>
2025-09-23 23:21:32 +0100

    quick fix on trophy to show a toast, TODO check if you already have that fish

--- src/main/java/com/wdiscute/starcatcher/items/TrophySilver.java
+++ src/main/java/com/wdiscute/starcatcher/items/TrophySilver.java
@@ -4,13 +4,17 @@ import com.wdiscute.starcatcher.Starcatcher;
 import com.wdiscute.starcatcher.networkandstuff.FishCaughtCounter;
 import com.wdiscute.starcatcher.networkandstuff.FishProperties;
 import com.wdiscute.starcatcher.networkandstuff.ModDataAttachments;
+import com.wdiscute.starcatcher.networkandstuff.Payloads;
 import net.minecraft.core.Holder;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.network.PacketDistributor;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,10 +41,18 @@ public class TrophySilver extends Item
             FishProperties fp = optional.get().value();
 
             fishCounter.add(new FishCaughtCounter(fp.fish(), 1));
+
+            if(player instanceof ServerPlayer sp)
+            {
+                PacketDistributor.sendToPlayer(sp, new Payloads.FishCaughtPayload(new ItemStack(BuiltInRegistries.ITEM.get(fp.fish()))));
+            }
+
         }
 
         player.setData(ModDataAttachments.FISHES_CAUGHT, fishCounter);
 
-        return super.use(level, player, usedHand);
+
+
+        return InteractionResultHolder.success(player.getItemInHand(usedHand));
     }
 }
