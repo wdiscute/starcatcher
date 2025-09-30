8a4e3be5bccf8a9eb2829c1101acb65f6e5bd316
wd <lucasalvesbispo2@gmail.com>
2025-10-01 00:41:37 +0100

    added more biome tags

--- src/main/java/com/wdiscute/starcatcher/StarcatcherTags.java
+++ src/main/java/com/wdiscute/starcatcher/StarcatcherTags.java
@@ -14,4 +14,11 @@ public class StarcatcherTags
     public static final ResourceLocation IS_OCEAN = Starcatcher.rl("is_ocean");
     public static final ResourceLocation IS_RIVER = Starcatcher.rl("is_river");
 
+    public static final ResourceLocation IS_BEACH = Starcatcher.rl("is_beach");
+    public static final ResourceLocation IS_SWAMP = Starcatcher.rl("is_swamp");
+    public static final ResourceLocation IS_CHERRY_GROVE = Starcatcher.rl("is_cherry_grove");
+    public static final ResourceLocation IS_MUSHROOM_FIELDS = Starcatcher.rl("is_mushroom_fields");
+    public static final ResourceLocation IS_DARK_FOREST = Starcatcher.rl("is_dark_forest");
+
+
 }
