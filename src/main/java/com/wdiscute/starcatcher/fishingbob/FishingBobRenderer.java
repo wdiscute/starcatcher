ce92c9f23bc830fa1bd5d95321de738d0bf7a910
wd <lucasalvesbispo2@gmail.com>
2025-10-09 14:05:46 +0100

    work on fishing line renderer bug

--- src/main/java/com/wdiscute/starcatcher/fishingbob/FishingBobRenderer.java
+++ src/main/java/com/wdiscute/starcatcher/fishingbob/FishingBobRenderer.java
@@ -25,6 +25,9 @@ public class FishingBobRenderer extends EntityRenderer<FishingBobEntity>
 {
     protected FishingBobModel<FishingBobEntity> model;
 
+    private static final ResourceLocation TEXTURE_LOCATION = ResourceLocation.withDefaultNamespace("textures/entity/fishing_hook.png");
+    private static final RenderType RENDER_TYPE = RenderType.entityCutout(TEXTURE_LOCATION);
+
     public FishingBobRenderer(EntityRendererProvider.Context context)
     {
         super(context);
@@ -41,81 +44,62 @@ public class FishingBobRenderer extends EntityRenderer<FishingBobEntity>
     @Override
     public void render(FishingBobEntity fishingBobEntity, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int packedLight)
     {
-        poseStack.pushPose();
+//        poseStack.pushPose();
+//        poseStack.translate(0.0F, 1.5F, 0.0F);
+//        poseStack.scale(-1.0F, -1.0F, 1.0F);
+//        int color = 0xff9999;
+//        ItemStack bobber = fishingBobEntity.getData(ModDataAttachments.BOBBER).stack().copy();
+//        if (bobber.is(ModItems.COLORFUL_BOBBER))
+//        {
+//            //why is rendering so annoying
+//            color = bobber.get(ModDataComponents.BOBBER_COLOR).getColorAsInt();
+//        }
+//        VertexConsumer vertexbobber = buffer.getBuffer(this.model.renderType(this.getTextureLocation(fishingBobEntity)));
+//        this.model.renderToBuffer(poseStack, vertexbobber, packedLight, OverlayTexture.NO_OVERLAY, color);
+//        poseStack.popPose();
 
-        poseStack.translate(0.0F, 1.5F, 0.0F);
-        poseStack.scale(-1.0F, -1.0F, 1.0F);
 
-        int color = 0xff9999;
-
-        ItemStack bobber = fishingBobEntity.getData(ModDataAttachments.BOBBER).stack().copy();
-
-        if(bobber.is(ModItems.COLORFUL_BOBBER))
+        if (fishingBobEntity.getOwner() instanceof Player player)
         {
-            //why is rendering so annoying
-            color = bobber.get(ModDataComponents.BOBBER_COLOR).getColorAsInt();
-        }
 
-        VertexConsumer vertexconsumer = buffer.getBuffer(this.model.renderType(this.getTextureLocation(fishingBobEntity)));
-        this.model.renderToBuffer(poseStack, vertexconsumer, packedLight, OverlayTexture.NO_OVERLAY, color);
-
-        poseStack.popPose();
-
-        //render string
-        if(fishingBobEntity.getOwner() instanceof Player player)
-        {
             poseStack.pushPose();
-
+            poseStack.pushPose();
+            poseStack.scale(0.5F, 0.5F, 0.5F);
+            poseStack.mulPose(this.entityRenderDispatcher.cameraOrientation());
+            PoseStack.Pose posestack$pose = poseStack.last();
+            VertexConsumer vertexconsumer = buffer.getBuffer(RENDER_TYPE);
+            vertex(vertexconsumer, posestack$pose, packedLight, 0.0F, 0, 0, 1);
+            vertex(vertexconsumer, posestack$pose, packedLight, 1.0F, 0, 1, 1);
+            vertex(vertexconsumer, posestack$pose, packedLight, 1.0F, 1, 1, 0);
+            vertex(vertexconsumer, posestack$pose, packedLight, 0.0F, 1, 0, 0);
+            poseStack.popPose();
             float f = player.getAttackAnim(partialTicks);
             float f1 = Mth.sin(Mth.sqrt(f) * (float) Math.PI);
             Vec3 vec3 = this.getPlayerHandPos(player, f1, partialTicks);
-            Vec3 vec31 = fishingBobEntity.getPosition(partialTicks).add(0.0F, 0.25F, 0.0F);
+            Vec3 vec31 = fishingBobEntity.getPosition(partialTicks).add(0.0, 0.25, 0.0);
             float f2 = (float) (vec3.x - vec31.x);
             float f3 = (float) (vec3.y - vec31.y);
             float f4 = (float) (vec3.z - vec31.z);
             VertexConsumer vertexconsumer1 = buffer.getBuffer(RenderType.lineStrip());
             PoseStack.Pose posestack$pose1 = poseStack.last();
+            int i = 16;
 
-            for (int j = 0; j <= 16; ++j)
+            for (int j = 0; j <= 16; j++)
             {
                 stringVertex(f2, f3, f4, vertexconsumer1, posestack$pose1, fraction(j, 16), fraction(j + 1, 16));
             }
 
-            //RenderSystem.setShaderColor(1, 1, 1, 1);
             poseStack.popPose();
-        }
-    }
+            super.render(fishingBobEntity, entityYaw, partialTicks, poseStack, buffer, packedLight);
 
-    private static float fraction(int numerator, int denominator) {
-        return (float)numerator / (float)denominator;
-    }
-
-    private Vec3 getPlayerHandPos(Player player, float p_340872_, float partialTick) {
-        int i = player.getMainArm() == HumanoidArm.RIGHT ? 1 : -1;
-        ItemStack itemstack = player.getMainHandItem();
-        if (!itemstack.is(ModItems.ROD)) {
-            i = -i;
         }
 
-        if (this.entityRenderDispatcher.options.getCameraType().isFirstPerson() && player == Minecraft.getInstance().player) {
-            double d4 = (double)960.0F / (double) this.entityRenderDispatcher.options.fov().get();
-            Vec3 vec3 = this.entityRenderDispatcher.camera.getNearPlane().getPointOnPlane((float)i * 0.525F, -0.1F).scale(d4).yRot(p_340872_ * 0.5F).xRot(-p_340872_ * 0.7F);
-            return player.getEyePosition(partialTick).add(vec3);
-        } else {
-            float f = Mth.lerp(partialTick, player.yBodyRotO, player.yBodyRot) * ((float)Math.PI / 180F);
-            double d0 = Mth.sin(f);
-            double d1 = Mth.cos(f);
-            float f1 = player.getScale();
-            double d2 = (double)i * 0.35 * (double)f1;
-            double d3 = 0.8 * (double)f1;
-            float f2 = player.isCrouching() ? -0.1875F : 0.0F;
-            return player.getEyePosition(partialTick).add(-d1 * d2 - d0 * d3, (double)f2 - 0.45 * (double)f1, -d0 * d2 + d1 * d3);
-        }
+
     }
 
-
-    private static void stringVertex(float x, float y, float z, VertexConsumer consumer, PoseStack.Pose pose, float stringFraction, float nextStringFraction)
-    {
+    private static void stringVertex(
+            float x, float y, float z, VertexConsumer consumer, PoseStack.Pose pose, float stringFraction, float nextStringFraction
+    ) {
         float f = x * stringFraction;
         float f1 = y * (stringFraction * stringFraction + stringFraction) * 0.5F + 0.25F;
         float f2 = z * stringFraction;
@@ -128,5 +112,48 @@ public class FishingBobRenderer extends EntityRenderer<FishingBobEntity>
         f5 /= f6;
         consumer.addVertex(pose, f, f1, f2).setColor(-16777216).setNormal(pose, f3, f4, f5);
     }
+
+    private static float fraction(int numerator, int denominator) {
+        return (float)numerator / (float)denominator;
+    }
+
+    private static void vertex(VertexConsumer consumer, PoseStack.Pose pose, int packedLight, float x, int y, int u, int v) {
+        consumer.addVertex(pose, x - 0.5F, (float)y - 0.5F, 0.0F)
+                .setColor(-1)
+                .setUv((float)u, (float)v)
+                .setOverlay(OverlayTexture.NO_OVERLAY)
+                .setLight(packedLight)
+                .setNormal(pose, 0.0F, 1.0F, 0.0F);
+    }
+
+    private Vec3 getPlayerHandPos(Player player, float p_340872_, float partialTick) {
+        int i = player.getMainArm() == HumanoidArm.RIGHT ? 1 : -1;
+        ItemStack itemstack = player.getMainHandItem();
+        if (!itemstack.is(ModItems.ROD)) {
+            i = -i;
+        }
+
+        if (this.entityRenderDispatcher.options.getCameraType().isFirstPerson() && player == Minecraft.getInstance().player) {
+            double d4 = 960.0 / (double)this.entityRenderDispatcher.options.fov().get().intValue();
+            Vec3 vec3 = this.entityRenderDispatcher
+                    .camera
+                    .getNearPlane()
+                    .getPointOnPlane((float)i * 0.525F, -0.1F)
+                    .scale(d4)
+                    .yRot(p_340872_ * 0.5F)
+                    .xRot(-p_340872_ * 0.7F);
+            return player.getEyePosition(partialTick).add(vec3);
+        } else {
+            float f = Mth.lerp(partialTick, player.yBodyRotO, player.yBodyRot) * (float) (Math.PI / 180.0);
+            double d0 = (double)Mth.sin(f);
+            double d1 = (double)Mth.cos(f);
+            float f1 = player.getScale();
+            double d2 = (double)i * 0.35 * (double)f1;
+            double d3 = 0.8 * (double)f1;
+            float f2 = player.isCrouching() ? -0.1875F : 0.0F;
+            return player.getEyePosition(partialTick).add(-d1 * d2 - d0 * d3, (double)f2 - 0.45 * (double)f1, -d0 * d2 + d1 * d3);
+        }
+    }
+
 
 }
