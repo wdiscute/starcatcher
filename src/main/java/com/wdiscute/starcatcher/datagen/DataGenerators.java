163f8858f02e8547fdc792d599ba50b7eeeefe15
wd <lucasalvesbispo2@gmail.com>
2025-10-23 16:54:26 +0100

    changed to datagenned item models

--- src/main/java/com/wdiscute/starcatcher/datagen/DataGenerators.java
+++ src/main/java/com/wdiscute/starcatcher/datagen/DataGenerators.java
@@ -1,9 +1,14 @@
 package com.wdiscute.starcatcher.datagen;
 
 import com.wdiscute.starcatcher.Starcatcher;
+import net.minecraft.data.DataGenerator;
 import net.minecraft.data.DataProvider;
+import net.minecraft.data.tags.ItemTagsProvider;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.model.generators.BlockModelProvider;
+import net.neoforged.neoforge.client.model.generators.ItemModelBuilder;
+import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
 import net.neoforged.neoforge.common.data.BlockTagsProvider;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
@@ -14,22 +19,25 @@ public class DataGenerators
     @SubscribeEvent
     public static void gatherData(GatherDataEvent event)
     {
+        DataGenerator gen = event.getGenerator();
+
         //fish properties
-        event.getGenerator().addProvider(
+        gen.addProvider(
                 event.includeServer(),
                 (DataProvider.Factory<FishAndTrophiesPropertiesProvider>) output -> new FishAndTrophiesPropertiesProvider(output,
                        event.getLookupProvider())
         );
 
-        BlockTagsProvider btp = new ModBlocksTagProvider(event.getGenerator().getPackOutput(), event.getLookupProvider(), event.getExistingFileHelper());
+        //item models
+        gen.addProvider(event.includeServer(), new ModItemModelProvider(gen.getPackOutput(), event.getExistingFileHelper()));
 
-        event.getGenerator().addProvider(event.includeServer(), btp);
+        //block tags
+        BlockTagsProvider btp = new ModBlocksTagProvider(gen.getPackOutput(), event.getLookupProvider(), event.getExistingFileHelper());
+        gen.addProvider(event.includeServer(), btp);
 
-        event.getGenerator().addProvider(event.includeServer(),
-                new ModItemsTagProvider(event.getGenerator().getPackOutput(),
-                                event.getLookupProvider(),
-                        btp.contentsGetter(),
-                        event.getExistingFileHelper()));
+        //item tags
+        ItemTagsProvider itp = new ModItemsTagProvider(gen.getPackOutput(), event.getLookupProvider(), btp.contentsGetter(), event.getExistingFileHelper());
+        gen.addProvider(event.includeServer(), itp);
     }
 
 
