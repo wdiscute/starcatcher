a653fcbf6094018bd28ec362ace184ae96d8a964
wd <lucasalvesbispo2@gmail.com>
2025-11-20 16:22:14 +0000

    added emi support

---
+++ src/main/java/com/wdiscute/starcatcher/emi/StarcatcherEmiRecipe.java
@@ -0,0 +1,133 @@
+package com.wdiscute.starcatcher.emi;
+
+import com.wdiscute.starcatcher.ModItems;
+import com.wdiscute.starcatcher.Starcatcher;
+import com.wdiscute.starcatcher.networkandcodecs.FishProperties;
+import com.wdiscute.starcatcher.networkandcodecs.ModDataComponents;
+import com.wdiscute.starcatcher.networkandcodecs.TrophyProperties;
+import dev.emi.emi.api.recipe.EmiRecipe;
+import dev.emi.emi.api.recipe.EmiRecipeCategory;
+import dev.emi.emi.api.render.EmiTexture;
+import dev.emi.emi.api.stack.EmiIngredient;
+import dev.emi.emi.api.stack.EmiStack;
+import dev.emi.emi.api.widget.WidgetHolder;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.ExtraCodecs;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class StarcatcherEmiRecipe implements EmiRecipe
+{
+
+    private final ResourceLocation id;
+    private final List<EmiStack> output;
+    private final FishProperties fp;
+    private final TrophyProperties tp;
+    private static final List<EmiIngredient> INPUT = List.of(
+            EmiIngredient.of(Ingredient.of(ModItems.GUIDE)),
+            EmiIngredient.of(Ingredient.of(ModItems.ROD)));
+
+    public StarcatcherEmiRecipe(ResourceLocation id, FishProperties fp)
+    {
+        this.output = List.of(EmiStack.of(fp.fish().value()));
+        this.id = id;
+        this.fp = fp;
+        this.tp = null;
+    }
+
+    public StarcatcherEmiRecipe(ResourceLocation id, TrophyProperties tp)
+    {
+        this.output = List.of(EmiStack.of(tp.fp().fish().value()));
+        this.id = id;
+        this.fp = tp.fp();
+        this.tp = tp;
+    }
+
+    @Override
+    public EmiRecipeCategory getCategory()
+    {
+        return StarcatcherEmiPlugin.STARCATCHER_CATEGORY;
+    }
+
+    @Override
+    public @Nullable ResourceLocation getId()
+    {
+        return Starcatcher.rl("/" + id.getPath());
+    }
+
+    @Override
+    public List<EmiIngredient> getInputs()
+    {
+        return List.of();
+    }
+
+    @Override
+    public List<EmiIngredient> getCatalysts()
+    {
+        return INPUT;
+    }
+
+    @Override
+    public List<EmiStack> getOutputs()
+    {
+        return output;
+    }
+
+    @Override
+    public int getDisplayWidth()
+    {
+        return 100;
+    }
+
+    @Override
+    public int getDisplayHeight()
+    {
+        return 22;
+    }
+
+
+    @Override
+    public void addWidgets(WidgetHolder widgets)
+    {
+
+        // Adds an input slot on the left
+        widgets.addSlot(INPUT.get(0), 5, 2);
+        widgets.addSlot(INPUT.get(1), 23, 2);
+
+        // Add an arrow texture to indicate processing
+        widgets.addTexture(EmiTexture.EMPTY_ARROW, 45, 2);
+
+        ItemStack is = new ItemStack(fp.fish());
+
+        if (!this.fp.customName().equals(FishProperties.DEFAULT.customName()))
+        {
+            is.set(DataComponents.ITEM_NAME, Component.translatable(fp.customName()));
+        }
+
+        if (this.tp != null)
+        {
+            if (!this.tp.customName().equals(TrophyProperties.DEFAULT.customName()) && tp.trophyType().equals(TrophyProperties.TrophyType.TROPHY))
+            {
+                is.set(DataComponents.ITEM_NAME, Component.translatable(tp.customName()));
+            }
+
+            is.set(ModDataComponents.TROPHY, this.tp);
+        }
+
+
+        widgets.addSlot(EmiIngredient.of(Ingredient.of(is)), 73, 2).recipeContext(this);
+
+
+        //widgets.addText(Component.translatable("emi.starcatcher.guide"), 0, 0, 0xffffff, true);
+
+        // Adds an output slot on the right
+        // Note that output slots need to call `recipeContext` to inform EMI about their recipe context
+        // This includes being able to resolve recipe trees, favorite stacks with recipe context, and more
+    }
+}
---
+++ src/main/java/com/wdiscute/starcatcher/emi/StarcatcherEmiRecipeCategory.java
@@ -0,0 +1,15 @@
+package com.wdiscute.starcatcher.emi;
+
+import dev.emi.emi.api.recipe.EmiRecipeCategory;
+import dev.emi.emi.api.render.EmiRenderable;
+import net.minecraft.resources.ResourceLocation;
+
+public class StarcatcherEmiRecipeCategory extends EmiRecipeCategory
+{
+
+
+    public StarcatcherEmiRecipeCategory(ResourceLocation id, EmiRenderable icon, EmiRenderable simplified)
+    {
+        super(id, icon, simplified);
+    }
+}
