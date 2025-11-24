e3fe9cf18708a20098bf0d1b6bc79f1c22751422
wd <lucasalvesbispo2@gmail.com>
2025-11-24 13:41:03 +0000

    added minigame helper

---
+++ src/main/java/com/wdiscute/starcatcher/minigame/FakeStarcatcherMinigameScreen.java
@@ -0,0 +1,59 @@
+package com.wdiscute.starcatcher.minigame;
+
+import com.wdiscute.starcatcher.ModItems;
+import com.wdiscute.starcatcher.networkandcodecs.FishProperties;
+import com.wdiscute.starcatcher.networkandcodecs.ModDataComponents;
+import com.wdiscute.starcatcher.networkandcodecs.SingleStackContainer;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.ItemStack;
+
+public class FakeStarcatcherMinigameScreen extends FishingMinigameScreen
+{
+
+    public final MinigameOnCloseFunction onCloseFunction;
+
+    @Override
+    public void tick()
+    {
+        if (completionSmooth > 75)
+        {
+            //if completed treasure minigame, or is a perfect catch with the mossy hook
+            boolean awardTreasure = treasureProgress > 100 || (perfectCatch && hook.is(ModItems.MOSSY_HOOK));
+            onCloseFunction.onClose(this.itemBeingFished.getItem(), this.fp, tickCount, awardTreasure, perfectCatch, consecutiveHits);
+
+            Minecraft.getInstance().options.guiScale().set(previousGuiScale);
+            this.minecraft.popGuiLayer();
+        }
+
+        super.tick();
+    }
+
+    @Override
+    public void onClose()
+    {
+        this.onCloseFunction.onClose(this.itemBeingFished.getItem(), this.fp, this.tickCount, false, false, this.consecutiveHits);
+        Minecraft.getInstance().options.guiScale().set(previousGuiScale);
+        this.minecraft.popGuiLayer();
+    }
+
+    public FakeStarcatcherMinigameScreen(FishProperties fp, ItemStack rod, MinigameOnCloseFunction onCloseFunction)
+    {
+        super(fp, rod);
+        this.onCloseFunction = onCloseFunction;
+    }
+
+    public FakeStarcatcherMinigameScreen(FishProperties fishProperties, ItemStack bobber, ItemStack bait, ItemStack hook, MinigameOnCloseFunction onCloseFunction)
+    {
+        this(fishProperties, makeRod(bobber, bait, hook), onCloseFunction);
+    }
+
+    private static ItemStack makeRod(ItemStack bobber, ItemStack bait, ItemStack hook)
+    {
+        ItemStack rod = new ItemStack(ModItems.ROD.get());
+
+        rod.set(ModDataComponents.BOBBER, new SingleStackContainer(bobber));
+        rod.set(ModDataComponents.BAIT, new SingleStackContainer(bait));
+        rod.set(ModDataComponents.HOOK, new SingleStackContainer(hook));
+        return rod;
+    }
+}
---
+++ src/main/java/com/wdiscute/starcatcher/minigame/MinigameOnCloseFunction.java
@@ -0,0 +1,9 @@
+package com.wdiscute.starcatcher.minigame;
+
+import com.wdiscute.starcatcher.networkandcodecs.FishProperties;
+import net.minecraft.world.item.Item;
+
+public interface MinigameOnCloseFunction
+{
+    void onClose(Item fish, FishProperties fp, int timeInTicks, boolean completedTreasure, boolean perfectCatch, int maxConsecutiveHits);
+}
