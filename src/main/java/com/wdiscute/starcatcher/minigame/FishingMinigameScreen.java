dcc2dd1ec31006add7633e980d8bfbff7d95ae96
wd <lucasalvesbispo2@gmail.com>
2025-11-25 22:32:34 +0000

    bump to v1.4, nerfed vanishing rate of legendary and rare

--- src/main/java/com/wdiscute/starcatcher/minigame/FishingMinigameScreen.java
+++ src/main/java/com/wdiscute/starcatcher/minigame/FishingMinigameScreen.java
@@ -201,8 +201,8 @@ public class FishingMinigameScreen extends Screen implements GuiEventListener
         if (isVanishing)
         {
             if (commonUncommonRareFish) vanishingRate = 0.03f;
-            if (fp.rarity().equals(FishProperties.Rarity.EPIC)) vanishingRate = 0.1f;
-            if (fp.rarity().equals(FishProperties.Rarity.LEGENDARY)) vanishingRate = 0.2f;
+            if (fp.rarity().equals(FishProperties.Rarity.EPIC)) vanishingRate = 0.06f;
+            if (fp.rarity().equals(FishProperties.Rarity.LEGENDARY)) vanishingRate = 0.1f;
 
             if (bobber.is(ModItems.CLEAR_BOBBER.get())) vanishingRate /= 2;
         }
