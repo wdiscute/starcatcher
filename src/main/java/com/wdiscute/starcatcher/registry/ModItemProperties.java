979571a8d2f229299af10bd9254f0c406d337bc0
wd <lucasalvesbispo2@gmail.com>
2025-11-30 15:53:02 +0000

    added cast property to every rod

--- src/main/java/com/wdiscute/starcatcher/registry/ModItemProperties.java
+++ src/main/java/com/wdiscute/starcatcher/registry/ModItemProperties.java
@@ -3,21 +3,27 @@ package com.wdiscute.starcatcher.registry;
 import com.wdiscute.starcatcher.Starcatcher;
 import com.wdiscute.starcatcher.io.ModDataAttachments;
 import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.world.item.Item;
+import net.neoforged.neoforge.registries.DeferredHolder;
 
 public class ModItemProperties
 {
 
-    public static void addCustomItemProperties() {
-        ItemProperties.register(
-                ModItems.ROD.get(),
-                Starcatcher.rl("cast"),
-                (stack, level, entity, seed) ->
-                {
-                    if(entity == null) return 0.0f;
-                    return  !entity.getData(ModDataAttachments.FISHING).isEmpty() && (entity.getMainHandItem() == stack || (entity.getOffhandItem() == stack)) ? 1.0f : 0.0f;
-                }
-        );
+    public static void addCustomItemProperties()
+    {
+
+        for (DeferredHolder<Item, ? extends Item> item : ModItems.RODS_REGISTRY.getEntries())
+        {
+            ItemProperties.register(
+                    item.get(),
+                    Starcatcher.rl("cast"),
+                    (stack, level, entity, seed) ->
+                    {
+                        if(entity == null) return 0.0f;
+                        return  !entity.getData(ModDataAttachments.FISHING).isEmpty() && (entity.getMainHandItem() == stack || (entity.getOffhandItem() == stack)) ? 1.0f : 0.0f;
+                    }
+            );
+        }
+
     }
-
-
 }
