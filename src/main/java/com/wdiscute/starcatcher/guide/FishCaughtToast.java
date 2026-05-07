e0ba48e26d1956b84fdc5a2603335fffa4f100a9
wd <lucasalvesbispo2@gmail.com>
2026-05-07 17:31:24 +0100

    fixed ETA rainbow values

--- src/main/java/com/wdiscute/starcatcher/guide/FishCaughtToast.java
+++ src/main/java/com/wdiscute/starcatcher/guide/FishCaughtToast.java
@@ -67,7 +67,7 @@ public class FishCaughtToast implements Toast
         guiGraphics.text(Minecraft.getInstance().font, this.title, 40, 13, 0xff635040, false);
 
         //todo ember text api lib missing
-        Component comp = Component.literal(/*"<sctoast>" +*/ rarity.wrapWithRarityMarkdownAsString(description) /*+ "</sctoast>"*/);
+        Component comp = Component.literal("<sctoast>" + rarity.wrapWithRarityMarkdownAsString(description) + "</sctoast>");
 
         guiGraphics.text(Minecraft.getInstance().font, comp, 40, 22, 0xff635040, false);
     }
