4ab5cf23ba2d3f53fc4f8dea1e9707b1e5687d09
Nikdo53 <usivatel@gmail.com>
2026-08-02 10:27:43 +0200

    Legacy4J patch

--- src/main/java/com/wdiscute/starcatcher/io/attachments/FishingBobAttachment.java
+++ src/main/java/com/wdiscute/starcatcher/io/attachments/FishingBobAttachment.java
@@ -3,6 +3,7 @@ package com.wdiscute.starcatcher.io.attachments;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.wdiscute.starcatcher.io.SCDataAttachments;
+import net.minecraftforge.common.capabilities.CapabilityProvider;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.nikdo53.neobackports.io.StreamCodec;
 import net.nikdo53.neobackports.io.utils.ByteBufCodecs;
@@ -34,7 +35,7 @@ public class FishingBobAttachment
         return uuid.isEmpty();
     }
 
-    public void setUuid(ICapabilityProvider holder, UUID uuid) {
+    public void setUuid(CapabilityProvider holder, UUID uuid) {
         this.uuid = uuid.toString();
         holder.setData(SCDataAttachments.FISHING_BOB, new FishingBobAttachment(this.uuid));
     //    holder.syncData(SCDataAttachments.FISHING_BOB);
