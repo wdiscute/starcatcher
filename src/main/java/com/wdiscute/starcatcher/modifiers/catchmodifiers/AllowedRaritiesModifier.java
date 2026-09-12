76afa5827cf5b7f027881d21ef0f7c8f92760f73
wd <lucasalvesbispo2@gmail.com>
2026-09-12 04:51:30 +0100

    added allowed rarities modifier

---
+++ src/main/java/com/wdiscute/starcatcher/modifiers/catchmodifiers/AllowedRaritiesModifier.java
@@ -0,0 +1,74 @@
+package com.wdiscute.starcatcher.modifiers.catchmodifiers;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import com.wdiscute.starcatcher.Starcatcher;
+import com.wdiscute.starcatcher.fish.Rarity;
+import com.wdiscute.starcatcher.modifiers.Modifier;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.List;
+
+public class AllowedRaritiesModifier extends AbstractCatchModifier
+{
+    final List<Rarity> rarities;
+
+    public static final MapCodec<AllowedRaritiesModifier> CODEC = RecordCodecBuilder.mapCodec(instance ->
+            instance.group(
+                    Rarity.CODEC.listOf().fieldOf("allowed_rarities").forGetter(o -> o.rarities),
+                    Codec.STRING.optionalFieldOf("translation_override", "").forGetter(o -> o.translationOverride)
+            ).apply(instance, AllowedRaritiesModifier::new));
+
+    @Override
+    public ResourceLocation getIdentifier()
+    {
+        return Starcatcher.rl("allowed_rarities");
+    }
+
+    @Override
+    public MapCodec<? extends Modifier> getCodec()
+    {
+        return CODEC;
+    }
+
+    public AllowedRaritiesModifier(List<Rarity> rarities, String translationOverride)
+    {
+        super(translationOverride);
+        this.rarities = rarities;
+    }
+
+    @Override
+    public List<Component> getNonOverriddenDescription(boolean shift)
+    {
+        Component text;
+
+        if (rarities.size() == 1)
+        {
+            text = Component.translatable(rarities.get(0).getSerializedName());
+        }
+        else
+        {
+            MutableComponent list = Component.empty();
+
+            for (int i = 0; i < rarities.size(); i++)
+            {
+                if (i > 0)
+                {
+                    list.append(Component.translatable(
+                            i == rarities.size() - 1
+                                    ? "tooltip.modifier.starcatcher.allowed_rarities.list_last_separator"
+                                    : "tooltip.modifier.starcatcher.allowed_rarities.list_separator"
+                    ));
+                }
+
+                list.append(Component.translatable(rarities.get(i).getSerializedName()));
+            }
+            text = list;
+        }
+
+        return List.of(Component.translatable("tooltip.modifier.starcatcher.allowed_rarities", text));
+    }
+}
